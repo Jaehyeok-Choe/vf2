34e18ee5ada5fa27aa77692cd9b39d6e1fff17be
tbeben77 <tbeben77@gmail.com>
2021-02-14 12:09:38 +0900

    kings 예약서비스 웹 작업

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    <v-app-bar-nav-icon @click="drawer = !drawer"/>
+      <Title :title="title"/>
+      <v-spacer/>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <Menu/>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+  <Footer :footer="footer"/>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Title from '@/views/site/Title'
+import Footer from '@/views/site/Footer'
+import Menu from '@/views/site/Menu'
 
-#nav {
-  padding: 30px;
+export default {
+  components: {
+    Title,
+    Footer,
+    Menu
+  },
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      title: 'KINGS (한국전력 국제원자력대학원대학교)',
+      footer: 'My footer'
+    }
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ src/views/site/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <v-footer padless color="primary" dark>
+        <v-col
+        class="text-center"
+        cols="12"
+        >
+        {{ new Date().getFullYear() + ' ' + footer}} — <strong>Vuetify</strong>
+        </v-col>
+    </v-footer>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['footer']
+}
+</script>
+
+<style>
+
+</style>
